9c4772fbb5cc592f6d0f9e8136c965cddc63c2e9
remixdrama <gerald.calotes@gmail.com>
2023-01-25 22:45:17 +0800

    added amplify

--- sulogoon_app/lib/main.dart
+++ sulogoon_app/lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:sulogoon_app/amplifyconfiguration.dart';
 import 'login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureAmplify();
   runApp(const SulogoonApp());
 }
+
+Future<void> configureAmplify() async {
+  // AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+  // Amplify.configure([authPlugin]);
+  Amplify.addPlugins([AmplifyAuthCognito()]);
+
+  try {
+    await Amplify.configure(amplifyconfig);
+  } catch (e) {
+    print("Amplify is already configured");
+  }
+}
+
+//ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+// content: Text("Confirmation complete"),));
 
 class SulogoonApp extends StatelessWidget {
   const SulogoonApp({super.key});
