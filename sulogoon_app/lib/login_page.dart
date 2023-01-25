8cfd6c1a9a61ff1fdf93e440327b99ea52863375
remixdrama <gerald.calotes@gmail.com>
2023-01-25 14:41:01 +0800

    install amplify

--- sulogoon_app/lib/login_page.dart
+++ sulogoon_app/lib/login_page.dart
@@ -9,97 +9,112 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //text controllers
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void btnTap() {
+    print("onTap from function");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[100],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //logo here
-              SizedBox(
-                  width: 500.0,
-                  height: 150.0,
-                  child: Image.asset('assets/Sulogoonlogo.png')),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo here
+                SizedBox(
+                    width: 500.0,
+                    height: 150.0,
+                    child: Image.asset('assets/Sulogoonlogo.png')),
 
-              SizedBox(
-                height: 30,
-              ),
-              //username textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 35),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black87),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.verified_user),
-                        border: InputBorder.none,
-                        hintText: 'Username',
+                SizedBox(
+                  height: 30,
+                ),
+                //username textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black87),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5.0),
+                      child: TextField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.verified_user),
+                          border: InputBorder.none,
+                          hintText: 'Username',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              //password textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 35),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black87),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.password),
-                        border: InputBorder.none,
-                        hintText: 'Password',
+                SizedBox(
+                  height: 10,
+                ),
+                //password textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black87),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.password),
+                          border: InputBorder.none,
+                          hintText: 'Password',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
+                SizedBox(
+                  height: 20,
+                ),
 
-              //sign in button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 35.0),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.black87,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Sign In',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                //sign in button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35.0),
+                  child: GestureDetector(
+                    onTap: btnTap,
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.black87,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
                       ),
                     ),
                   ),
+                  //sign up link
                 ),
-                //sign up link
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
